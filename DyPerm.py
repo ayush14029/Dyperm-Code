3bfc075a4522c7431f7b99e444cfe84993aaac61
ayush14029 <ayush14029@iiitd.ac.in>
2025-10-15 01:17:14 -0700

    new test

--- DyPerm.py
+++ DyPerm.py
@@ -4,7 +4,7 @@ from sklearn.metrics.cluster import adjusted_rand_score
 import operator
 from collections import defaultdict
 import time
-G=nx.Graph()a
+G=nx.Graph()ab
 global comm_list
 comm_list=[]
 
